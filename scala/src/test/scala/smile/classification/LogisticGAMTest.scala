fbbfe1f0d05e196525bb468853d7737edc3e4427
RileyOShea <rileyoshea@fico.com>
2019-05-03 17:13:09 -0700

    Refactor test code.

--- scala/src/test/scala/smile/classification/LogisticGAMTest.scala
+++ scala/src/test/scala/smile/classification/LogisticGAMTest.scala
@@ -1,7 +1,7 @@
 package smile.classification
 
+import io.circe.Json
 import io.circe.parser._
-import io.circe.{HCursor, Json}
 import org.junit.Assert._
 import org.junit.Test
 import smile.classification.LogisticGAM.BSpline.bSplineBasis
@@ -16,47 +16,20 @@ class LogisticGAMTest {
 
   @Test
   def testBSplineBasis(): Unit = {
-    // Read test data
-    val json: Json = parse(
-      Source
-        .fromResource("classification/LogisticGAM/bSplineTestData.json")
-        .getLines()
-        .foldLeft("")((acc, x) => acc + x)
-    ).getOrElse(Json.Null)
-
-    assert(json != Json.Null, "Could not parse test data.")
-
     for {
-      arr <- json.asArray
-      (data, iter) <- arr.zipWithIndex
+      inputs <- Utils.readTestData()
+      (basis, x, nSplines, splineOrder, edgeKnots) <- inputs
     } {
-      val cursor: HCursor = data.hcursor
-      (
-        cursor.get[Array[Array[Double]]]("basis"),
-        cursor.get[Array[Double]]("x"),
-        cursor.get[Int]("nSplines"),
-        cursor.get[Int]("splineOrder"),
-        cursor.get[(Double, Double)]("edgeKnots")
-      ) match {
-        case (Right(basis),
-        Right(x),
-        Right(nSplines),
-        Right(splineOrder),
-        Right(edgeKnots)) =>
-          val basisTest = bSplineBasis(x, edgeKnots, nSplines, splineOrder)
-          basis.zip(basisTest).foreach {
-            case (row, rowTest) => assertArrayEquals(row, rowTest, 0.000000001)
-          }
-
-        case _ =>
-          assert(assertion = false, "Failed to decode test data from Json.")
+      val basisTest = bSplineBasis(x, edgeKnots, nSplines, splineOrder)
+      basis.zip(basisTest).foreach {
+        case (row, rowTest) => assertArrayEquals(row, rowTest, 0.000000001)
       }
     }
   }
 
   @Test(expected = classOf[IllegalArgumentException])
   def testBSplineBasisInvalidNSplines(): Unit = {
-    val (x, edgeKnots) = randomInputData()
+    val (x, edgeKnots) = Utils.randomInputData()
     val nSplines = 0
     val splineOrder = 1
     bSplineBasis(x, edgeKnots, nSplines, splineOrder)
@@ -65,27 +38,68 @@ class LogisticGAMTest {
 
   @Test(expected = classOf[IllegalArgumentException])
   def testBSplineBasisInvalidSplineOrder(): Unit = {
-    val (x, edgeKnots) = randomInputData()
+    val (x, edgeKnots) = Utils.randomInputData()
     val nSplines = 2
     val splineOrder = -1
     bSplineBasis(x, edgeKnots, nSplines, splineOrder)
     ()
   }
 
-  def randomInputData(): (Array[Double], (Double, Double)) = {
-    val rng = new Random(1)
-    val x = (for {_ <- 10 to rng.nextInt(100)} yield rng.nextDouble()).toArray
-    val edgeKnots = (x.min, x.max)
-    (x, edgeKnots)
-  }
-
   @Test(expected = classOf[IllegalArgumentException])
   def testBSplineBasisInvalidSplineOrderWithNSplines(): Unit = {
-    val (x, edgeKnots) = randomInputData()
+    val (x, edgeKnots) = Utils.randomInputData()
     val nSplines = 4
     val splineOrder = 4
     bSplineBasis(x, edgeKnots, nSplines, splineOrder)
     ()
   }
+}
+
+private object Utils {
+
+  type bSplineData =
+    (Array[Array[Double]], Array[Double], Int, Int, (Double, Double))
+
+  def randomInputData(): (Array[Double], (Double, Double)) = {
+    val rng = new Random(1)
+    val x =
+      (for { _ <- 10 to rng.nextInt(100) } yield rng.nextDouble()).toArray
+    val edgeKnots = (x.min, x.max)
+    (x, edgeKnots)
+  }
+
+  def readTestData(): Option[Vector[bSplineData]] =
+    readJsonFromFile().asArray flatMap { jsons =>
+      Some((jsons map extractFromJson) flatMap (_.toList))
+    }
+
+  def readJsonFromFile(): Json = {
+    val json: Json = parse(
+      Source
+        .fromResource("classification/LogisticGAM/bSplineTestData.json")
+        .getLines()
+        .foldLeft("")((acc, x) => acc + x)
+    ).getOrElse(Json.Null)
+
+    assert(json != Json.Null, "Could not parse test data.")
+    json
+  }
+
+  def extractFromJson(json: Json): Option[bSplineData] =
+    (
+      json.hcursor.get[Array[Array[Double]]]("basis"),
+      json.hcursor.get[Array[Double]]("x"),
+      json.hcursor.get[Int]("nSplines"),
+      json.hcursor.get[Int]("splineOrder"),
+      json.hcursor.get[(Double, Double)]("edgeKnots")
+    ) match {
+      case (Right(basis),
+            Right(x),
+            Right(nSplines),
+            Right(splineOrder),
+            Right(edgeKnots)) =>
+        Some((basis, x, nSplines, splineOrder, edgeKnots))
+      case _ => None
+    }
 
 }
